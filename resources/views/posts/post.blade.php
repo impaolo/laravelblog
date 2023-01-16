936a6a26a087514057ac6b8e867076b57f851f8b
Paolo Siroko <paolosiroko@Paolos-MacBook-Pro.local>
2023-01-16 17:41:45 +0300

    crud u

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -5,11 +5,11 @@
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                     <img src="/images/illustration-1.png" alt="" class="rounded-xl">
-                    @foreach ($posts as $post)
+                   
                     <p class="mt-4 block text-gray-400 text-xs">
                         Published <time>{{ $post->created_at->diffForHumans(); }}</time>
                     </p>
-                    @endforeach
+                    
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
